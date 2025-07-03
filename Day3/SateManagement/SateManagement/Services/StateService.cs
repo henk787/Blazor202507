f7de43b06be46d97e4ef26bd94fbdd045ceb6b8c
Henk Holterman <HenkH@4dotnet.nl>
2025-07-03 09:51:22 +0200

    state

---
+++ Day3/SateManagement/SateManagement/Services/StateService.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace SateManagement.Services
+{
+    public class StateService(ProtectedSessionStorage ProtectedSessionStore)
+    {        
+        public ValueTask SetCounter(int counterValue)
+        {            
+            return ProtectedSessionStore.SetAsync(nameof(StateService), "Counter", counterValue);
+        }
+
+        public async ValueTask<int> GetCounter()
+        {            
+            var result = await ProtectedSessionStore.GetAsync<int>(nameof(StateService), "Counter");
+
+            return result.Success ? result.Value : 0;
+        }
+
+    }
+}
